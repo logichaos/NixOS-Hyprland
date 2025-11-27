4ca5cbb62b8ad872aea5c7add1f45010af69f78a
installer <installer@gmail.com>
2025-11-27 18:50:55 +0100

    home(devtools): expose dotnet-9 and dotnet-10 wrappers to avoid build conflicts

--- modules/home/devtools/default.nix
+++ modules/home/devtools/default.nix
@@ -4,12 +4,18 @@
  , pkgs
  , ... }:
 
-{
+let
+	dotnet9 = pkgs.dotnetCorePackages.sdk_9_0-bin;
+	dotnet10 = pkgs.dotnetCorePackages.sdk_10_0-bin;
+	dotnet9Wrapper = pkgs.writeShellScriptBin "dotnet-9" ''exec "${dotnet9}/bin/dotnet" "$@"'';
+	dotnet10Wrapper = pkgs.writeShellScriptBin "dotnet-10" ''exec "${dotnet10}/bin/dotnet" "$@"'';
+in {
 	# Provide .NET SDK 9 and 10 in the user profile for developers who need them.
-	# These package names follow the pattern used elsewhere in this repo (hosts/*).
+	# Wrap the SDK executables with versioned names to avoid conflicting
+	# `/bin/dotnet` entries when both SDKs are present in a buildEnv.
 	home.packages = with pkgs; [
-		dotnetCorePackages.sdk_9_0-bin
-		dotnetCorePackages.sdk_10_0-bin
+		dotnet9Wrapper
+		dotnet10Wrapper
 	];
 
 }
