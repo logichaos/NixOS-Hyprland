fb8c807d1a712e76dbfc02e3af8620b2ed652e02
Logichaos <logichaoscodes@gmail.com>
2025-11-30 14:05:01 +0100

    add default-user template user

---
+++ users/default-user/variables.nix
@@ -0,0 +1,15 @@
+# 💫 https://github.com/JaKooLit 💫 #
+# User-specific variables for default-user (template)
+
+{
+  # Git Configuration
+  gitUsername = "noneset";
+  gitEmail = "noneset@gmail.com";
+
+  # Program Preferences
+  browser = "vivaldi"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  
+  # Waybar Settings
+  clock24h = true;
+}
